f98af706f5bcbcd696dd03c6e792f85162f1831f
Yuri Sampaio <sampaio.yuri31@gmail.com>
2022-05-03 18:45:29 -0300

    construindo o futureBuilder

--- main.dart
+++ main.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; // permite que fazemos as requisições 
+import 'package:http/http.dart' as http; // permite que fazemos as requisições
 import 'dart:async'; // permite que fazemos as requisições mas não fiquemos as esperamos
 import 'dart:convert'; // library para transformar os dados em json
 
-
 // URL de requisição
-const request = "https://api.hgbrasil.com/finance?format=json&key=7daf723a "; //API
+const request =
+    "https://api.hgbrasil.com/finance?format=json&key=7daf723a "; //API
 void main() async {
-
-  print(await getData()); // pega os dados da função getData 
+  print(await getData()); // pega os dados da função getData
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false, // tira o debug no canto da tela
-    
+
     home: Home(),
+     theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white
+    )
   ));
 }
 
-Future<Map> getData() async{
+Future<Map> getData() async {
   // requisição assíncrona
   //resposta da API
-  http.Response  response = await http.get(Uri.parse(request)); // manda o get para o servidor com a URL
+  http.Response response = await http
+      .get(Uri.parse(request)); // manda o get para o servidor com a URL
   return json.decode(response.body);
-} 
+}
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -37,7 +41,7 @@ class HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Conversor',
+          '\$ Conversor \$',
           style: TextStyle(
             color: Colors.black,
           ),
@@ -47,91 +51,129 @@ class HomeState extends State<Home> {
         // botão para apagar as informações nos campos de textos
       ),
       backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          // validação
-          child: Form(
-            child: Column(
-              children: [
-                Center(
-                  child: Icon(
-                    Icons.attach_money_rounded,
-                    color: Colors.amber,
-                    size: 100,
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando dados",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                //
-                //
-                // TEXTFORMFIELD
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Reais',
-                    labelStyle: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 25,
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      "Erro ao carrega dados dados",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.amber,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                //
-                //
-                // TEXTFORMFIELD
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Dólares',
-                    labelStyle: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 25,
+                  );
+                } else {
+                  var dollar =
+                      snapshot.data!["results"]["currencies"]["USD"]["buy"];
+
+                  var euro =
+                      snapshot.data!["results"]["currencies"]["EUR"]["buy"];
+                  //tela que rola
+                  return SingleChildScrollView(
+                    
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      //
+                      //
+                      // validação
+                      child: Form(
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Icon(
+                                Icons.monetization_on,
+                                color: Colors.amber,
+                                size: 100,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            //
+                            //
+                            // TEXTFORMFIELD R$
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Reais',
+                                labelStyle: TextStyle(
+                                  color: Colors.amber,
+                                  fontSize: 25,
+                                ),
+                                prefixText: "R\$"
+                              ),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.amber,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            //
+                            //
+                            // TEXTFORMFIELD USD
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Dólares',
+                                labelStyle: TextStyle(
+                                  color: Colors.amber,
+                                  fontSize: 25,
+                                ),
+                                prefixText: "\$"
+                              ),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.amber,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            //
+                            //
+                            // TEXTFORMFIELD EUR
+                            TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Euro',
+                                labelStyle: TextStyle(
+                                  color: Colors.amber,
+                                  fontSize: 25,
+                                ),
+                                prefixText: "€"
+                              ),
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.amber,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.amber,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                //
-                //
-                // TEXTFORMFIELD
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Euro',
-                    labelStyle: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 25,
-                    ),
-                  ),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.amber,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+                  );
+                }
+            }
+          }),
     );
   }
 }
